557213e3bc03d4bc68e13f42cf55cea5e148d040
soulas <helloworldwithsoulas@gmail.com>
2026-07-18 15:39:04 +0900

    static company detail page

---
+++ app/companies/[id]/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>会社名</h1>
+        <h2>rank</h2>
+        <p>ID</p>
+        <p>ステータス</p>
+        <h2>TODO</h2>
+        <h3>応募</h3>
+        <h2>予約</h2>
+        <h3>1945/04/04~ MTG</h3>
+        <p>mypage</p>
+        <p>mypage ID</p>
+        <p>オファー元サイト</p>
+        <p>年収予定</p>
+        <p>副業可 リモート可 自社開発 セキュリティ</p>
+        <p>平均残業時間</p>
+        <p>その他メモ</p>
+        <p>作成日時</p>
+        <p>変更日時</p>
+      </main>
+    </div>
+  );
+}
