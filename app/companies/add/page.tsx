1cfaf4ffd33c592e606040abfde8b6e74d8aa914
soulas <helloworldwithsoulas@gmail.com>
2026-07-22 12:40:36 +0900

    working add form.

--- app/companies/add/page.tsx
+++ app/companies/add/page.tsx
@@ -1,3 +1,4 @@
+"use server";
 import { FieldGroup, FieldSet, Field, FieldLabel } from "@/components/ui/field";
 
 import { Input } from "@/components/ui/input";
@@ -5,49 +6,73 @@ import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 
 import { Button } from "@/components/ui/button";
+import { addCompany } from "@/lib/data-access";
 
-export default function Home() {
+export default async function Home() {
+  const add = async (formData: FormData) => {
+    "use server";
+
+    console.log(formData.get("canSide"));
+    // validation
+    if (!formData.get("name")) return;
+    if (!formData.get("rank")) return;
+
+    await addCompany({
+      // must (brank strings are logically unreachable)
+      name: formData.get("name")?.toString() || "",
+      rank: formData.get("rank")?.toString() || "",
+
+      // not have to
+      mypageUrl: formData.get("mypageurl")?.toString() || "",
+      mypageId: formData.get("mypageid")?.toString() || "",
+      offeredFrom: formData.get("rank")?.toString() || "",
+      salary: formData.get("salary")?.toString() || "",
+      canSideJob: formData.get("canSide")?.toString() === "on",
+      canRemote: formData.get("canRemote")?.toString() === "on",
+      haveOwnProduct: formData.get("haveProduct")?.toString() === "on",
+      haveSecurity: formData.get("haveSerucity")?.toString() === "on",
+      avgOvertime: parseInt(formData.get("avgOvertime")?.toString() || "-1"),
+
+      note: formData.get("note")?.toString() || "",
+    });
+  };
+
   return (
     <div>
-      <form>
+      <form action={add}>
         <FieldGroup>
           <FieldSet>
             <FieldGroup>
               <Field>
                 <FieldLabel>会社名</FieldLabel>
-                <Input></Input>
+                <Input name="name"></Input>
                 <FieldLabel>ランク</FieldLabel>
-                <Input></Input>
+                <Input name="rank"></Input>
                 <FieldLabel>ステータス</FieldLabel>
-                <Input></Input>
-                <FieldLabel>TODO</FieldLabel>
-                <Input></Input>
-                <FieldLabel>応募</FieldLabel>
-                <Input></Input>
-                <FieldLabel>予約</FieldLabel>
-                <Input></Input>
+                <Input name="status"></Input>
                 <FieldLabel>mypage</FieldLabel>
-                <Input></Input>
+                <Input name="mypageurl"></Input>
                 <FieldLabel>mypageid</FieldLabel>
-                <Input></Input>
+                <Input name="mypageid"></Input>
                 <FieldLabel>オファー元サイト</FieldLabel>
-                <Input></Input>
+                <Input name="offeredfrom"></Input>
                 <FieldLabel>年収予定</FieldLabel>
-                <Input></Input>
+                <Input name="salary"></Input>
                 <FieldLabel>副業</FieldLabel>
-                <Checkbox />
+                <Checkbox name="canSide" />
                 <FieldLabel>リモート</FieldLabel>
-                <Checkbox />
+                <Checkbox name="canRemote" />
                 <FieldLabel>自社開発</FieldLabel>
-                <Checkbox />
+                <Checkbox name="haveProduct" />
                 <FieldLabel>セキュリティ</FieldLabel>
-                <Checkbox />
-                <Input></Input>
+                <Checkbox name="haveSecurity" />
                 <FieldLabel>平均残業時間</FieldLabel>
-                <Input></Input>
+                <Input name="avgOvertime"></Input>
                 <FieldLabel>そのほかメモ</FieldLabel>
-                <Input></Input>
-                <Button variant="outline">Submit</Button>
+                <Input name="note"></Input>
+                <Button variant="outline" type="submit">
+                  Submit
+                </Button>
               </Field>
             </FieldGroup>
           </FieldSet>
--- lib/data-access.ts
+++ lib/data-access.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "./generated/prisma/client";
 
 import { PrismaPg } from "@prisma/adapter-pg";
 
-export const adapter = new PrismaPg({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
@@ -16,6 +16,39 @@ export async function getCompanyFromId(id: number) {
   return prisma.company.findUnique({
     where: {
       id,
+    },
+  });
+}
+
+export async function addCompany(params: {
+  name: string;
+  rank: string;
+  mypageUrl?: string;
+  mypageId?: string;
+  offeredFrom?: string;
+  salary?: string;
+  canSideJob?: boolean;
+  canRemote?: boolean;
+  haveOwnProduct?: boolean;
+  haveSecurity?: boolean;
+  avgOvertime?: number;
+  note?: string;
+}) {
+  const prisma = new PrismaClient({ adapter });
+  await prisma.company.create({
+    data: {
+      name: params.name,
+      rank: params.rank,
+      mypageUrl: params.mypageUrl,
+      mypageId: params.mypageId,
+      offeredFrom: params.offeredFrom,
+      salary: params.salary,
+      canSideJob: params.canSideJob,
+      canRemote: params.canRemote,
+      haveOwnProduct: params.haveOwnProduct,
+      haveSecurity: params.haveSecurity,
+      avgOvertime: params.avgOvertime,
+      note: params.note,
     },
   });
 }
